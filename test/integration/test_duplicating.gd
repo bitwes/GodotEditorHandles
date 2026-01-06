d7638c0f8257eaa8914227c7ce0feedfe8edf4b7
Butch Wesley <butch.wesley@gmail.com>
2026-01-06 13:45:58 -0500

    code review

--- test/integration/test_duplicating.gd
+++ test/integration/test_duplicating.gd
@@ -3,29 +3,10 @@ extends GutTest
 
 class NodeWithHandles:
 	extends Node
-	var _engine = Engine
+	var _Engine = Engine
 	@export var editor_handles : EditorHandles :
 		set(val):
 			editor_handles = EditorHandles.create_or_copy_resource(self, val)
-
-
-	# func _init() -> void:
-	# 	print('NEW ', self, ' ', editor_handles)
-	# 	print_stack()
-
-
-	func _ready() -> void:
-		if(_engine.is_editor_hint()):
-			# print("HERE ", self)
-			# editor_handles.editor_setup(self)
-			editor_handles.resized.connect(_on_editor_handles_resized)
-
-
-	func _on_editor_handles_resized():
-		pass
-		# print(self, ' resized')
-
-
 
 
 func test_duplicate_gets_new_editor_handles():
@@ -33,14 +14,14 @@ func test_duplicate_gets_new_editor_handles():
 	stub(engine_dbl, "is_editor_hint").to_return(true)
 
 	var orig = autofree(NodeWithHandles.new())
-	orig._engine = engine_dbl
+	orig._Engine = engine_dbl
 	orig.editor_handles = EditorHandles.new()
 	add_child(orig)
 
 	await wait_idle_frames(2)
-	# print("------- ----------- -----------")
+
 	var dupe = autofree(orig.duplicate())
-	dupe._engine = engine_dbl
+	dupe._Engine = engine_dbl
 	add_child(dupe)
 
 	assert_ne(dupe.editor_handles, orig.editor_handles)
